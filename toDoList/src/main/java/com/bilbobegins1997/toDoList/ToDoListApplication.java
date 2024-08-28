13c11412c5ae45cc9fc2e4e44a437f6005c5a342
Vlad Axelrod <32596508+bilbobegins@users.noreply.github.com>
2024-08-28 17:14:32 +0200

    Initial commit

---
+++ toDoList/src/main/java/com/bilbobegins1997/toDoList/ToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.bilbobegins1997.toDoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListApplication.class, args);
+	}
+
+}
---
+++ toDoList/src/test/java/com/bilbobegins1997/toDoList/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilbobegins1997.toDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
